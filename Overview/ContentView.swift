e513fe7c0d24b669f6ae92bd0f7df871b3ffd85f
Jonathan Bones <bonesyblue@gmail.com>
2022-01-04 20:44:46 +0100

    feat: Create a simple counter application

--- Overview/ContentView.swift
+++ Overview/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var counter = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(action: { self.counter += 1 }, label: {
+                Text("Tap me!")
+                    .padding()
+                    .background(Color(.tertiarySystemFill))
+                    .cornerRadius(5)
+            })
+            
+            if counter > 0 {
+                Text("You've tapped \(counter) times")
+            } else {
+                Text("You've not yet tapped")
+            }
+        }
     }
 }
 
